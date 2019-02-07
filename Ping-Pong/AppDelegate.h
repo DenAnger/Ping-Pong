c6c16d2f5c857db36ca1cd1c639b7c8a380dcc03
DenAnger <DenAnger@outlook.com>
2019-02-07 19:35:15 +0700

    Sum
    Sum

---
+++ Ping-Pong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ping-Pong
+//
+//  Created by Denis Abramov on 02/02/2019.
+//  Copyright © 2019 Denis Abramov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ping-Pong/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  Ping-Pong
+//
+//  Created by Denis Abramov on 02/02/2019.
+//  Copyright © 2019 Denis Abramov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+
+@end
+
